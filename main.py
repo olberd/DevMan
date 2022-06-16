1a3cdf5e7fe4f90996f61b53b28f142b37097ab8
olberd <olberd@bk.ru>
2022-06-17 00:41:07 +0300

    Hid global variables

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
-def shorten_link(token, link):
+def shorten_link(token, bitly_url, link):
     headers = {
       'Authorization': f'Bearer {token}'
     }
     data = {
       'long_url': link,
     }
-    response = requests.post(BITLY_URL, headers=headers, json=data)
+    response = requests.post(bitly_url, headers=headers, json=data)
     response.raise_for_status()
     short_link = response.json()
     return short_link['link']
 
 
-def count_clicks(token, link):
+def count_clicks(token, bitly_url, link):
     link_parse = urlparse(link)
     link = f'{link_parse.netloc}{link_parse.path}'
-    url_for_link = f'{BITLY_URL}{link}/clicks/summary'
+    url_for_link = f'{bitly_url}{link}/clicks/summary'
     headers = {
       'Authorization': f'Bearer {token}'
     }
@@ -32,36 +32,36 @@ def count_clicks(token, link):
     return clicks_count
 
 
-def is_bitlink(token, link):
+def is_bitlink(token, bitly_url, link):
     headers = {
       'Authorization': f'Bearer {token}'
     }
     parsed_url = urlparse(link)
     link = f'{parsed_url.netloc}{parsed_url.path}'
-    response = requests.get(f'{BITLY_URL}{link}', headers=headers)
+    response = requests.get(f'{bitly_url}{link}', headers=headers)
     return response.ok
 
 
 def main():
+    load_dotenv()
+    BITLY_TOKEN = os.environ['TOKEN_BITLY']
+    BITLY_URL = 'https://api-ssl.bitly.com/v4/bitlinks/'
     parser = argparse.ArgumentParser(description='Сокращение ссылок')
     parser.add_argument('link', help='Введите ссылку')
     args = parser.parse_args()
     link = args.link
     try:
-        if is_bitlink(BITLY_TOKEN, link):
+        if is_bitlink(BITLY_TOKEN, BITLY_URL, link):
             print(
                 'По вашей ссылке прошли:',
-                count_clicks(BITLY_TOKEN, link),
+                count_clicks(BITLY_TOKEN, BITLY_URL, link),
                 'раз(а)'
             )
         else:
-            print('Битлинк', shorten_link(BITLY_TOKEN, link))
+            print('Битлинк', shorten_link(BITLY_TOKEN, BITLY_URL, link))
     except requests.exceptions.HTTPError:
         print('Исправьте ссылку')
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    BITLY_TOKEN = os.environ['TOKEN_BITLY']
-    BITLY_URL = 'https://api-ssl.bitly.com/v4/bitlinks/'
     main()
